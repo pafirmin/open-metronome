38782d89fb6a19728f9782a39afed81d6908a6bc
Paul <paulfirmin1@gmail.com>
2023-03-14 20:24:05 +0000

    Create metronome component

---
+++ src/components/metronome/Metronome.tsx
@@ -0,0 +1,72 @@
+import styled from "@emotion/styled";
+import { css, keyframes } from '@emotion/react'
+import { useMetronome } from "../../hooks";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 300px;
+  margin: auto;
+`;
+
+const TempoDisplay = styled.span`
+  display: block;
+  font-size: 3rem;
+  text-align: center;
+`;
+
+const swing = keyframes`
+  0% {
+    transform: rotate(-45deg);
+  }
+  100% {
+    transform: rotate(45deg);
+  }
+`;
+
+type PendulumProps = {
+  isPlaying: boolean
+  tempo: number
+};
+
+const Pendulum = styled.div<PendulumProps>`
+  width: 2px;
+  height: 80%;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  background-color: #c4c4c4;
+  transform-origin: bottom center;
+  animation: ${(props) =>
+    props.isPlaying &&
+    css`
+      ${swing} ${60 / props.tempo}s linear infinite alternate
+    `};
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: ${(props) => ((props.tempo - 50) / 168) * 100}%;
+    left: 50%;
+    transform: translate(-50%);
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #c4c4c4;
+    transition: top 0.5s;
+  }
+`;
+
+const Metronome = () => {
+  const { tempo, isPlaying } = useMetronome()
+  console.log(tempo)
+
+  return (
+    <Wrapper>
+      <TempoDisplay>{tempo}bpm</TempoDisplay>
+      <Pendulum isPlaying={isPlaying} tempo={tempo} />
+    </Wrapper>
+  );
+};
+
+export default Metronome

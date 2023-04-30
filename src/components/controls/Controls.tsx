acf3b8063e97bec451df2d1f5cc9fcd6b46dc30e
Paul <paulfirmin1@gmail.com>
2023-04-30 23:35:34 +0100

    style updates

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -1,135 +1,77 @@
 import styled from "@emotion/styled";
 import { useMetronome } from "../../hooks";
-import { useEffect, useState } from "react";
-import { IconButton, Slider } from "@mui/material";
-import AddIcon from "@mui/icons-material/AddCircleOutline";
-import RemoveIcon from "@mui/icons-material/RemoveCircleOutline";
+import { SlMusicToneAlt, SlMusicTone } from "react-icons/sl";
+import { IconButton } from "../common";
+import { useTheme } from "@emotion/react";
+import { Stack } from "../common";
+import TapTempo from "./TapTempo";
 
 const Wrapper = styled.div`
   margin: 1rem auto 0 auto;
   text-align: center;
-  width: 100%;
-`;
-
-const ControlContainer = styled.div`
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  font-size: 1.4rem;
-  margin: 0 auto;
+  align-items: center;
+  margintop: 1rem;
 `;
 
 const StartBtn = styled.button`
   border-radius: 0;
   font-size: 1.6em;
   padding: 0.6rem 1rem;
-  background: #425eca;
+  background: ${({ theme }) => theme.colors.primary.main};
   font: inherit;
   color: inherit;
   border: 1px solid #bdbdbd;
   cursor: pointer;
   width: 100px;
+  height: 50px;
 
   &:hover {
     background: #33489b;
   }
 `;
 
-const NoteValue = styled.span<{ selected: boolean }>`
-  font-size: 1.8em;
-  cursor: pointer;
-  color: ${(props) => (props.selected ? "red" : "inherit")};
-`;
-
 const Controls = () => {
-  const { values, setValues, isRunning, reset, startPulse, isLoading } =
-    useMetronome();
-  const [sliderVal, setSliderVal] = useState(values.tempo);
-
-  const handleTempo = (val: number) => {
-    setValues((prev) => ({ ...prev, tempo: val }));
-  };
-
-  const handleAdjustMetre = (change: number) => {
-    setValues((prev) => ({ ...prev, metre: prev.metre + change }));
-  };
+  const { values, setValues, isRunning, reset, startPulse } = useMetronome();
+  const theme = useTheme();
 
   const handleNoteValue = (val: 1 | 0.5) => {
     setValues((prev) => ({ ...prev, division: val }));
   };
 
-  useEffect(() => {
-    setSliderVal(values.tempo);
-  }, [values.tempo]);
-
   return (
     <Wrapper>
-      <Slider
-        value={sliderVal}
-        min={Number(import.meta.env.VITE_MIN_TEMPO)}
-        max={Number(import.meta.env.VITE_MAX_TEMPO)}
-        onChange={(_e, newVal) => setSliderVal(newVal as number)}
-        onChangeCommitted={(_e, val) => handleTempo(val as number)}
-        valueLabelDisplay="auto"
-        valueLabelFormat={sliderVal + " bpm"}
-        color="secondary"
-      />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginTop: "1rem",
-        }}
-      >
-        <div style={{ width: "150px" }}>
-          <span style={{ display: "block" }}>Metre</span>
-          <ControlContainer>
-            <IconButton
-              color="inherit"
-              onClick={() => handleAdjustMetre(-1)}
-              aria-label="decrement meter"
-              size="large"
-            >
-              <RemoveIcon fontSize="large" />
-            </IconButton>
-            <span>{values.metre}</span>
-            <IconButton
-              color="inherit"
-              onClick={() => handleAdjustMetre(1)}
-              aria-label="increment meter"
-              size="large"
-            >
-              <AddIcon fontSize="large" />
-            </IconButton>
-          </ControlContainer>
-        </div>
-        <StartBtn onClick={isRunning ? reset : startPulse}>
-          {isLoading ? "initialising... " : isRunning ? "Stop" : "Start"}
-        </StartBtn>
-        <div style={{ width: "150px" }}>
-          <span style={{ display: "block" }}>Note</span>
-          <ControlContainer>
-            <NoteValue
-              onClick={() => handleNoteValue(1)}
-              role="button"
-              tabIndex={0}
-              aria-label="Quarter note pulse"
-              selected={values.division === 1}
-            >
-              ♩
-            </NoteValue>
-            <NoteValue
-              onClick={() => handleNoteValue(0.5)}
-              role="button"
-              tabIndex={0}
-              aria-label="Eighth note pulse"
-              selected={values.division === 0.5}
-            >
-              ♫
-            </NoteValue>
-          </ControlContainer>
-        </div>
+      <Stack alignItems="center" style={{ width: "150px" }}>
+        <TapTempo />
+      </Stack>
+      <StartBtn onClick={isRunning ? reset : startPulse}>
+        {isRunning ? "Stop" : "Start"}
+      </StartBtn>
+      <div style={{ width: "150px" }}>
+        <span style={{ display: "block" }}>Note</span>
+        <Stack direction="row" justifyContent="space-between">
+          <IconButton
+            aria-label="quarter note pulse"
+            onClick={() => handleNoteValue(1)}
+          >
+            <SlMusicTone
+              color={
+                values.division === 1 ? theme.colors.accent.main : "inherit"
+              }
+            />
+          </IconButton>
+          <IconButton
+            aria-label="8th note pulse"
+            onClick={() => handleNoteValue(0.5)}
+          >
+            <SlMusicToneAlt
+              color={
+                values.division === 0.5 ? theme.colors.accent.main : "inherit"
+              }
+            />
+          </IconButton>
+        </Stack>
       </div>
     </Wrapper>
   );
--- src/components/custom-program/ProgramChunkForm.tsx
+++ src/components/custom-program/ProgramChunkForm.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
-import { Button } from "@mui/material";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import ReactSlider from "react-slider";
 import { ProgramChunk } from "../../common/interfaces/program-chunk.interface";
 
 interface Props {
@@ -11,17 +11,71 @@ const ProgramForm = styled.form`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 2rem;
   gap: 0.5rem;
   font-size: 1.2rem;
 `;
 
 const NumberInput = styled.input`
-  width: 50px;
-  height: 20px;
+  width: 65px;
+  height: 35px;
+  text-align: center;
+  padding: 0.4rem;
+  font-size: inherit;
+  border: none;
+`;
+
+const TempoPopover = styled.div<{
+  top: number;
+  left: number;
+  visible: boolean;
+}>`
+  position: fixed;
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  opacity: ${(props) => (props.visible ? "100" : "0")};
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  transform: translateX(-50%);
+  transition: opacity 0.3s, visibility 0.3s;
+  z-index: 1000;
+
+  &:hover {
+    visibility: visible;
+    opacity: 100%;
+  }
+`;
+
+const PopoverContent = styled.div`
+  position: relative;
+  background: #ffffffc4;
+  box-shadow: 0px 0px 8px 3px #2c2c2c;
+  margin-top: 20px;
+  width: 200px;
+  padding: 0.8rem;
+  box-sizing: border-box;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: -12px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 0 12px 12px 12px;
+    border-color: transparent transparent #ffffffc4 transparent;
+  }
 `;
 
 const ProgramChunkForm = ({ onSubmit }: Props) => {
+  const tempoRef = useRef<HTMLInputElement>(null);
+  const [showSlider, setShowSlider] = useState(false);
+
+  const popoverTop = tempoRef.current?.getBoundingClientRect().bottom || 0;
+  const popoverLeft =
+    Number(tempoRef.current?.getBoundingClientRect().x) +
+    Number(tempoRef.current?.offsetWidth) / 2;
+
   const [chunkValues, setChunkValues] = useState<ProgramChunk>({
     id: "",
     measures: 1,
@@ -46,6 +100,8 @@ const ProgramChunkForm = ({ onSubmit }: Props) => {
     >
       <NumberInput
         type="number"
+        min={1}
+        max={1000}
         name="measures"
         value={chunkValues.measures}
         onChange={handleChange}
@@ -53,21 +109,41 @@ const ProgramChunkForm = ({ onSubmit }: Props) => {
       <span>bars of</span>
       <NumberInput
         type="number"
+        min={2}
+        max={32}
         name="metre"
         value={chunkValues.metre}
         onChange={handleChange}
       />
       <span>at</span>
       <NumberInput
+        ref={tempoRef}
+        min={import.meta.env.VITE_MIN_TEMPO}
+        max={import.meta.env.VITE_MAX_TEMPO}
         type="number"
         name="tempo"
         value={chunkValues.tempo}
+        onMouseOver={() => setShowSlider(true)}
+        onMouseLeave={() => setShowSlider(false)}
         onChange={handleChange}
       />
+      <TempoPopover top={popoverTop} left={popoverLeft} visible={showSlider}>
+        <PopoverContent>
+          <ReactSlider
+            value={chunkValues.tempo}
+            min={import.meta.env.VITE_MIN_TEMPO}
+            max={import.meta.env.VITE_MAX_TEMPO}
+            onChange={(num) => setChunkValues({ ...chunkValues, tempo: num })}
+            trackClassName={"sliderTrack"}
+            thumbClassName={"sliderThumb"}
+            className={"slider"}
+          />
+        </PopoverContent>
+      </TempoPopover>
       <span>bpm</span>
-      <Button type="submit" color="inherit" variant="outlined">
+      <button type="submit" color="inherit">
         add
-      </Button>
+      </button>
     </ProgramForm>
   );
 };

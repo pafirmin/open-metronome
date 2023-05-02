3ea449489627f69fa1f3aa839a93516e97f687f2
Paul <paulfirmin1@gmail.com>
2023-05-02 10:25:25 +0100

    prepare to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/open-metronome/",
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: './setup-tests.ts',
+    setupFiles: "./setup-tests.ts",
   },
-})
+});
